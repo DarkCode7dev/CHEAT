9ab1ffd9499187675f5b08a827c772e70b8630f0
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2021-09-24 11:24:35 +0530

    Create Login.cs

---
+++ GoalSetting/API/Models/Login.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models {
+    public class Login {
+         public string EmployeeName { get; set; }
+        public string Password { get; set; }
+    }
+}
